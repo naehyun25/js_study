9a4977e8f73e3a6401a61f311bda4adf2bf8ebfa
naehyun25 <yoon658@naver.com>
2022-12-23 19:41:17 +0900

    20221222:Review:변수자료형의응용'

---
+++ js/review02.js
@@ -0,0 +1,22 @@
+let name="nacho";
+document.write(`hello, ${name}`);
+document.write(`<br>hello${1+2}`);
+let coffe;
+coffe=true;
+let pucho;
+pucho=false;
+document.write(pucho);
+document.write(coffe);
+coffe=3>1;
+document.write(coffe);
+coffe=1<3;
+console.log(coffe);
+
+let great;
+great=4>1;
+console.log(great);
+document.write(`<br> ${great}`);
+document.write(great);
+//'', "" , `` 모두 문자열을 넣는데 `` 기호는 ${}안에 변수, 표현식을 넣으주면 문자열 중간에넣을수있다.
+document.write(typeof coffe);
+document.write(typeof(coffe));
