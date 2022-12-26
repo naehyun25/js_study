5f52c9df7744bd22b84a8559b6513b81cc42dca1
naehyun25 <yoon658@naver.com>
2022-12-26 15:15:29 +0900

    for문복습

---
+++ self-study/js/self01-05.js
@@ -0,0 +1,10 @@
+let year = prompt('몇년도에출간했을까요?');
+if (year==2015) {alert('딩동댕');
+document.write('wow')
+}
+else if(year>2015){alert('한번더')
+document.write('onemore')
+}
+else{alert('땡')
+document.write('woo')}
+
