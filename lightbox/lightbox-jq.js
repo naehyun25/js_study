dadc14f65f2fdb2b90f89f354beb3179d1ddead1
naehyun25 <yoon658@naver.com>
2023-01-26 22:07:59 +0900

    Review:tab/lightbox/report

--- lightbox/lightbox-jq.js
+++ lightbox/lightbox-jq.js
@@ -1,33 +1,33 @@
-// $(function(){
-//     var $button = $('.gallery li a'),
-//     $target=$('#lightbox-overlay'),
-//     $targetImg=$target.find('img');
+$(function(){
+    var $button = $('.gallery li a'),
+    $target=$('#lightbox-overlay'),
+    $targetImg=$target.find('img');
     
-//     $button.click(function(){
-//         var newImg = $(this).find('img').attr('data-lightbox');
-//         var oldImg = $(this).find('img').attr('src');
-//         $target.addClass('visible')
-//         //$targetImg.attr('src',oldImg)
-//         $targetImg.attr('src',newImg)
-//     })
-//     $target.click(function(){
-//          $target.removeClass('visible');
-//     })
-// })
+    $button.click(function(){
+        var newImg = $(this).find('img').attr('data-lightbox');
+        var oldImg = $(this).find('img').attr('src');
+        $target.addClass('visible')
+        //$targetImg.attr('src',oldImg)
+        $targetImg.attr('src',newImg)
+    })
+    $target.click(function(){
+         $target.removeClass('visible');
+    })
+})
 
-//내거
-// $(function(){
-//     let imgs = $('.gallery li a img');
-//     const layer = $('#lightbox-overlay');
-//     imgs.click(function(){
-//         layer.addClass('visible');
-//         let popImg = $(this).attr('data-lightbox');
-//         layer.find('img').attr('src',popImg);
-//     });
-//     layer.click(function(){
-//         layer.removeClass('visible');
-//     })
-// })
+내거
+$(function(){
+    let imgs = $('.gallery li a img');
+    const layer = $('#lightbox-overlay');
+    imgs.click(function(){
+        layer.addClass('visible');
+        let popImg = $(this).attr('data-lightbox');
+        layer.find('img').attr('src',popImg);
+    });
+    layer.click(function(){
+        layer.removeClass('visible');
+    })
+})
 
 addEventListener('DOMContentloaded',function(){
     console.log("hoi")
---
+++ review/js/lightbox_review.js
@@ -0,0 +1,30 @@
+// $(function(){
+//     const $button = $(".gallery li a"),
+//     $target = $("#lightbox-overlay"),
+//     $targetImg = $target.find("img");
+
+//     $button.click(function(){
+//         $target.addClass("visible");
+//         let newImg = $(this).find('img').attr('data-lightbox');
+//         $targetImg.attr('src',newImg);
+//     })
+//     $target.click(function(){
+//         $target.removeClass("visible");
+//     })
+// })
+addEventListener("DOMContentLoaded",function(){
+    const buttons = document.querySelectorAll('.gallery li a');
+    const target = document.querySelector("#lightbox-overlay");
+    const targetImg = document.querySelector("#lightbox-overlay img");
+    const t1 = document.querySelector(".gallery-container")
+    
+    for(i=0; i<buttons.length; i++)
+    buttons[i].onclick = function(){
+        target.classList.add("visible");
+        let newImg = this.firstChild.getAttribute("data-lightbox");
+        targetImg.setAttribute('src',newImg)
+    }
+    target.onclick = function(){
+        target.classList.remove("visible")
+    }
+})
