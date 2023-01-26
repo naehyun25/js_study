4afa2a6966ceee1846d97ed025e69a28b3b296bf
naehyun25 <yoon658@naver.com>
2023-01-26 15:36:36 +0900

    Class:20230126-lightbox/tab

--- review/js/accrodian-js.js
+++ review/js/accrodian-js.js
@@ -1,24 +1,30 @@
-let gnbliA = document.querySelectorAll(".gnb>li>a");
+let gnbLiA = document.querySelector(".gnb>li>a");
+console
+
+
+$(function(){
+let gnbliA = $(".gnb>li>a");
+console.log(gnbliA)
 gnbliA.click(function(){
     const sub = $('.sub');
     sub.removeClass('on');
     $(this).next().addClass("on");
 })
-
-function toggle(){ 
-    if(!$(this).next().hasClass("on"))
-    {sub.removeClass("on"),
-      $(this).next().addClass("on");
-  }else{sub.removeClass("on")}
-    };
+});
+// function toggle(){ 
+//     if(!$(this).next().hasClass("on"))
+//     {sub.removeClass("on"),
+//       $(this).next().addClass("on");
+//   }else{sub.removeClass("on")}
+//     };
 
     
-let check=false;
-let aH=$('.sub>li>a').outerHeight();
-console.log(aH)
-const gnbLiA = $(".gnb>li>a");
-gnbLiA.click(function(){    
-    const sub = $(".sub")
-sub.css('height',"0px")
-$(this).next().css("height",aH*4-12+"px");
-})
+// let check=false;
+// let aH=$('.sub>li>a').outerHeight();
+// console.log(aH)
+// const gnbLiA = $(".gnb>li>a");
+// gnbLiA.click(function(){    
+//     const sub = $(".sub")
+// sub.css('height',"0px")
+// $(this).next().css("height",aH*4-12+"px");
+// })
